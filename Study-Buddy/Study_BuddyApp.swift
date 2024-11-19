6ebb9b0b8836227672652305126148ae9736dbd2
Prince-Rosario <princerosario2003@gmail.com>
2024-11-19 15:45:02 +0700

    Create Project

---
+++ Study-Buddy/Study_BuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Study_BuddyApp.swift
+//  Study-Buddy
+//
+//  Created by Prince Rosario on 19/11/24.
+//
+
+import SwiftUI
+
+@main
+struct Study_BuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Study-BuddyTests/Study_BuddyTests.swift
@@ -0,0 +1,17 @@
+//
+//  Study_BuddyTests.swift
+//  Study-BuddyTests
+//
+//  Created by Prince Rosario on 19/11/24.
+//
+
+import Testing
+@testable import Study_Buddy
+
+struct Study_BuddyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
